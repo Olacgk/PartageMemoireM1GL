53079a937734237cb7a229f6b046c24171825e05
Olacgk <theotimechognika@gmail.com>
2024-06-02 08:29:35 +0000

    commentaire model & expert, memoire model

---
+++ MetierPM/Model/Expert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MetierPM.Model
+{
+    public class Expert:Personne
+    {
+        [Required, MaxLength(80)]
+        public string Specialite { get; set; }
+    }
+}
