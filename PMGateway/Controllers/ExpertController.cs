2dce14b74cd6e9709eee67869e0a64c8f7b12631
Theotime Chognika <94149002+Olacgk@users.noreply.github.com>
2024-06-29 17:57:41 +0000

    swagger error resolved

--- PMGateway/Controllers/ExpertController.cs
+++ PMGateway/Controllers/ExpertController.cs
@@ -9,9 +9,22 @@ namespace PMGateway.Controllers
     {
         ServiceMetier.Service1Client service = new ServiceMetier.Service1Client();
 
+        [HttpPost]
         public bool AddExpert(ServiceMetier.Expert expert)
         {
             return service.AddExpert(expert);
+        }
+
+        [HttpGet]
+        public ICollection<ServiceMetier.Expert> GetAllExpert()
+        {
+            return service.GetAllExpert();
+        }
+
+        [HttpDelete]
+        public bool DeleteExpert(ServiceMetier.Expert expert)
+        {
+            return service.DeleteExpert(expert.Id);
         }
     }
 }
