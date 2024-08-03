0cf6a3a5613690009e3184f4df8a7d1f25189a8b
Theotime Chognika <94149002+Olacgk@users.noreply.github.com>
2024-08-03 10:40:23 +0000

    Controller routes protect

--- PMGateway/Program.cs
+++ PMGateway/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 // Add services to the container.
@@ -6,6 +10,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Jwt settings
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["validIssuer"],
+        ValidAudience = jwtSettings["validAudience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+    .GetBytes(jwtSettings.GetSection("securityKey").Value))
+    };
+});
 
 var app = builder.Build();
 
@@ -26,6 +50,9 @@ app.UseCors(builder =>
            .SetPreflightMaxAge(TimeSpan.FromSeconds(3600));
 });
 
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
